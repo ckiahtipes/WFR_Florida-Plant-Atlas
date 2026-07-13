a61d073ef6acfa5bb1b5c7aca56321ef3aa96462
ckiahtipes <chris.kiahtipes@gmail.com>
2026-07-13 09:11:14 -0600

    Working out routine for searching large datasets for individual numbers.

---
+++ cultivar_search.R
@@ -0,0 +1,18 @@
+#Helping an Atlas user out by finding culivars in a list of plants.
+
+barcodes <- read.csv("data/AFP_barcodes.csv", header = TRUE)
+
+cult_pull <- read.csv("data/cult_mod_AFP.csv", header = TRUE)
+
+match = sapply(cult_pull$barcode, function(x){
+  grep(x, barcodes$SrcID, fixed = TRUE)
+})
+
+for(i in 1:length(match)){
+  if(length(match[[i]]) >= 1){
+    id_pull = barcodes$SrcID[match[[i]]]
+  } else {}
+  print(id_pull)
+}
+
+212950
