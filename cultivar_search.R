6dc61c1e6f50e78ddacf169e067e39198da4a11f
ckiahtipes <chris.kiahtipes@gmail.com>
2026-07-15 08:41:20 -0600

    Fixed logic to return matches for cultivars found through herbarium specimen search.

--- cultivar_search.R
+++ cultivar_search.R
@@ -4,16 +4,25 @@ barcodes <- read.csv("data/AFP_barcodes.csv", header = TRUE)
 
 cult_pull <- read.csv("data/cult_mod_AFP.csv", header = TRUE)
 
-match = apply(cult_pull$barcode, function(x){
+match = sapply(cult_pull$barcode, function(x){
   barcodes$SrcID[barcodes$SrcID == x]
 })
 
+cultivar_notes = vector("character", length = nrow(barcodes))
+barcodes = cbind(barcodes, cultivar_notes)
+cleanup_tracker = vector("logical", length = length(match))
 
+for(i in 1:length(match)){
+  if(length(match[[i]]) == 1){
+    barcodes$cultivar_notes[barcodes$SrcID==match[[i]]] = cult_pull$PlDescr[i]
+    cleanup_tracker[i] = TRUE
+  } else {
+    cleanup_tracker[i] = FALSE
+    #barcodes$cultivar_notes[barcodes$SrcID==match[[i]]] = NA
+  }
+}
 
-#for(i in 1:length(match)){
-#  if(length(match[[i]]) >= 1){
-#    id_pull = barcodes$SrcID[match[[i]]]
-#  } else {}
-#  print(id_pull)
-#}
+matched_table = cult_pull[cleanup_tracker == TRUE,]
 
+write.csv(matched_table, "data/cultivar_matches.csv")
+write.csv(barcodes, "data/barcode_notes.csv")
